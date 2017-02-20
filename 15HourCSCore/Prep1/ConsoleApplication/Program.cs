32ffefad0128df34ba65b3b658dbc6812d9e5091
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-02-20 15:57:18 -0600

    Trying to set up Rider.

---
+++ 15HourCSCore/Prep1/ConsoleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
