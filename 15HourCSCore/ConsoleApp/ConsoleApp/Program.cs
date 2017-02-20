952b3db7ed66592d04d58c92958e1afd321a1e3d
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-02-20 16:17:07 -0600

    Alright, install mono and .net successful.

--- 15HourCSCore/ConsoleApp/ConsoleApp/Program.cs
+++ 15HourCSCore/ConsoleApp/ConsoleApp/Program.cs
@@ -9,6 +9,17 @@ namespace ConsoleApp
     {
         public static void Main(string[] args)
         {
+            Console.Write("Hello, World!\n");
+
+            int[] numArr = {1, 2, 3, 4, 5, 6};
+
+            Console.Write(Equals(1, 2-2) ? "maybe\n" : "Nope\n");
+            Console.Write(Equals(1, 2 - 1) ? "Of Course it's true!\n" : "This shouldn't happen\n");
+
+            foreach (var n in numArr)
+            {
+                Console.Write(n);
+            }
         }
     }
 }
