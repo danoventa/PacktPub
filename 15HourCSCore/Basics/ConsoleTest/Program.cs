d26c66ca9f0c256edf92e891861c9b527bf2d676
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-02-28 08:59:18 -0600

    more testing with rider.

---
+++ 15HourCSCore/Basics/ConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleTest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
