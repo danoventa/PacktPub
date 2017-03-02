5c15a9b92c74351d05e2e3d5ad1a2fa9eb384547
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-03-01 22:34:23 -0600

    updating file.

--- 15HourCSCore/Basics/ConsoleTest/Program.cs
+++ 15HourCSCore/Basics/ConsoleTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace ConsoleTest
 {
@@ -7,6 +8,57 @@ namespace ConsoleTest
     {
         public static void Main(string[] args)
         {
+            var li = new List<int>();
+            li.Add(5);
+            li.Add(1);
+            li.Add(3);
+            li.Add(2);
+            li.Add(4);
+
+            wave(li);
+            for (var i = 0;
+                i < li.Count;
+                i++)
+                    {
+                        Console.Write(Convert.ToString(li[i]) + " " );
+                    }
+                }
+        public static List<int> wave(List<int> A) {
+            bool toggle = false;
+            int temp;
+
+            if(A.Count < 2){
+                return A;
+            }
+
+            for(var i = 0; i < A.Count - 1; i++){
+
+                print("Current index");
+                print(i);
+                if(toggle){
+                    if(A[i] > A[i + 1]){
+                        temp = A[i];
+                        A[i] = A[i + 1];
+                        A[i+1] = temp;
+                    }
+                    toggle = false;
+                }else{
+                    if(A[i] < A[i + 1]){
+                        temp = A[i];
+                        A[i] = A[i + 1];
+                        A[i+1] = temp;
+                    }
+                    toggle = true;
+                }
+                print(A[i]);
+
+            }
+            return A;
+        }
+
+        public static void print(object mess)
+        {
+            Console.Write(Convert.ToString(mess) + "\n");
         }
     }
 }
